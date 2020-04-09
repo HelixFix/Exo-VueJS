cad499581230ff620d748185bb998b3e4ef3ef8c
HelixFix <jerome.rinner@gmail.com>
2020-04-09 14:32:02 +0200

    todosComponentsCreated

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,40 @@
 
     <div id="app">
 
-        
+      <Todos /> <!--  Afficher Todos -->
 
     </div>
 
 </template>
 
 <script>
-
+import Todos from './components/Todos'; // Iportorter Todos
 
 export default {
   name: 'App',
   components: {
-    
+      Todos // Utiliser Todos
+  },
+  data () {
+      return {
+          todos: [
+              {
+                  id: 1,
+                  title: "Todo One",
+                  completed: false
+              },
+              {
+                  id: 2,
+                  title: "Todo Two",
+                  completed: true
+              },
+              {
+                  id: 3,
+                  title: "Todo Three",
+                  completeted: false
+              }
+          ]
+      }
   }
 }
 </script>
