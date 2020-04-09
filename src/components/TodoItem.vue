1e0ccf6d35810dd38d2c0082375fbd73bb9b519f
HelixFix <jerome.rinner@gmail.com>
2020-04-09 15:33:52 +0200

    createTodoItem

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,9 +1,38 @@
 <template>
 
-    <div>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
 
-        <p></p>
-        
+        <p> {{ todo.title }} </p> <!--Utilise le props todo dynamiquement pour afficher le titre -->
+
     </div>
 
 </template>
+
+<script>
+
+export default {
+
+    name: "TodoItem",
+    props: ["todo"] // Propriétés de todo
+    
+}
+
+</script>
+
+<style scoped>
+
+    .todo-item {
+
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+
+    }
+
+    .is-complete {
+
+        text-decoration: line-through;
+
+    }
+
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -3,7 +3,8 @@
 <div>
 
     <div v-bind:key="todo.id" v-for="todo in todos"> <!-- Boucle table ID Todo -->
-        <h3> {{ todo.title }} </h3> <!-- Affiche le titre de chaques éléments -->
+        <!-- <h3> {{ todo.title }} </h3> Affiche le titre de chaques éléments -->
+        <TodoItem v-bind:todo="todo"/> <!-- Afficher TodoItem -->
 
     </div>
 
@@ -12,10 +13,14 @@
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue' // Importer TodoItem
 
 export default {
     name: "Todos",
-    props: ["todos"]
+    components: {
+        TodoItem // Utiliser TodoItem
+    },
+    props: ["todos"] // Propriétés de todos
 }
 
 </script>
